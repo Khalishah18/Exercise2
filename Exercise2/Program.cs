21ac33c810621f80f5f17c50b09f1a54667ec790
LISA <ASUS@LAPTOP-67G15PD7>
2022-10-31 15:47:31 +0700

    Add number of elements in the array

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace Exercise2
         //Array to be searched //Max number of data 79+20-2x15+10+20 = 99
         int[] arr = new int[99];
 
+        //Number of elements in the array
+        int n;
         static void Main(string[] args)
         {
         }
