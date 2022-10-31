f0bea14beb011ec2b85cc8521b3b06f7fc3a6aa0
LISA <ASUS@LAPTOP-67G15PD7>
2022-10-31 16:12:22 +0700

    add loop to search for the elements in the array

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -60,7 +60,27 @@ namespace Exercise2
                 //obtain the index of the middle elements
                 int mid = (lowerbound + upperbound) / 2;
                 int ctr = 1;
-            }
+
+                //loop to search for the elements in the array
+                while ((item != arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + " found at position " + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + " not found iin the array \n");
+                Console.WriteLine("\nNumber of comparasion : " + ctr);
+
+                Console.Write("\nContinue Search (y/n):");
+                ch = char.Parse(Console.ReadLine());
+                ;
+            } while ((ch == 'y') || (ch == 'Y'));
         }
         static void Main(string[] args)
         {
