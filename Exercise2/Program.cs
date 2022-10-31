c7041833dc31332dc07e9bc6f671981a9553eea6
LISA <ASUS@LAPTOP-67G15PD7>
2022-10-31 16:25:00 +0700

    add accept the number to be searched

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -79,13 +79,37 @@ namespace Exercise2
 
                 Console.Write("\nContinue Search (y/n):");
                 ch = char.Parse(Console.ReadLine());
-                ;
+                
             } while ((ch == 'y') || (ch == 'Y'));
         }
         public void LinearSearch()
         {
             char ch;
+            //search for number of comparison
+            int ctr;
+            do
+            {
+                //accept the number to be searched
+                Console.Write("\nEnter the element you want to search: ");
+                int item = Convert.ToInt32((Console.ReadLine()));
 
+                ctr = 0;
+                for (i = 0; i < n; i++)
+                {
+                    ctr++;
+                    if (arr[i] == item)
+                    {
+                        Console.WriteLine("\n" + item.ToString() + "found st position " + (i + 1).ToString());
+                        break;
+                    }
+                    
+                }
+                if (i == n)
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array");
+                Console.WriteLine("\n Number of comparison " + ctr);
+                Console.WriteLine("\nContinue search (y/n):");
+                ch = char.Parse(Console.ReadLine());
+            } while ((ch == 'y') || (ch == 'Y'));
         }
         static void Main(string[] args)
         {
