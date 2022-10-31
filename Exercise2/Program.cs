fbda190b52e4913f4f3193af4292a4d96187d93e
LISA <ASUS@LAPTOP-67G15PD7>
2022-10-31 16:02:01 +0700

    add apply binary search

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -52,6 +52,10 @@ namespace Exercise2
                 //accept the number to be searched
                 Console.Write("\n Enter element want you to search : ");
                 int item = Convert.ToInt32(Console.ReadLine());
+
+                //apply binary search
+                int lowerbound = 0;
+                int upperbound = n - 1;
             }
         }
         static void Main(string[] args)
